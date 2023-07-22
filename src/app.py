5eefcf0f2fc4182b54d4f4cd41aa9a96e4b22107
Fariborz <fariborz.saffari@snapp.cab>
2023-07-22 14:45:46 +0330

    add multwallet with name

--- src/app.py
+++ src/app.py
@@ -1,23 +1,26 @@
-from flask import Flask
+from flask import Flask,jsonify
 from prometheus_client import start_http_server, Gauge
 from controller import GetBalance
 
 
 app = Flask(__name__)
 
-balance_metric = Gauge('bsc_balance', 'Balance of BSC wallet')
+balances_metric = Gauge('bsc_balance', 'Balance of BSC wallet', ['wallet_address'])
 
 @app.route('/metrics')
 def metrics():
-    try:
-        balance = GetBalance()
-        balance_metric.set(balance.get_balance())
-        return str(balance_metric.collect()[0])
-    except ValueError as e:
-        return str(e)
+    
+    bscbalance = GetBalance()
+    balances=bscbalance.get_balance()
+
+    for address, balance in balances.items():
+        if balance is not None:
+            balances_metric.labels(wallet_address=address).set(balance)
+
+    return str(balances_metric.collect())
 
 if __name__ == '__main__':
     # Start Prometheus metrics server on port 8000
     start_http_server(8015)
     # Start Flask web server
-    app.run()
+    app.run(host="0.0.0.0",port=5001)
--- src/controller.py
+++ src/controller.py
@@ -13,28 +13,42 @@ class GetBalance():
         if not self.api_key:
             raise ValueError("BSC_SCAN_API_KEY environment variable not set")
 
-        self.wallet_address = os.environ.get('WALLET_ADDRESS')
-        if not self.wallet_address:
-            raise ValueError("WALLET_ADDRESS environment variable not set")
+        #self.wallet_address = os.environ.get('WALLET_ADDRESS')
+        #if not self.wallet_address:
+        #    raise ValueError("WALLET_ADDRESS environment variable not set")
 
         # BscScan API request parameters
-        self.params = {
-            'module': 'account',
-            'action': 'balance',
-            'address': self.wallet_address,
-            'apikey': self.api_key,
-        }
-
+        self.wallet_addresses = [os.environ.get('WALLET_ADDRESS_1'), os.environ.get('WALLET_ADDRESS_2'), os.environ.get('WALLET_ADDRESS_3')]
 
     def get_balance(self):
+        balances = {}
+        for address in self.wallet_addresses:
+            # BscScan API request parameters
+            self.params = {
+                'module': 'account',
+                'action': 'balance',
+                'address': address,
+                'apikey': self.api_key,
+            }
+            print(self.params)
         
         # Make BscScan API request
-        response = requests.get(api_endpoint, params=self.params)
-        data = response.json()
+            response = requests.get(api_endpoint, params=self.params)
+            data = response.json()
+            print(data)
 
         # Check if the request was successful
-        if data['status'] == '1':
-            balance = int(data['result']) / 1e18  # Convert balance from wei to BNB
-            return balance
-        else:
-            raise ValueError(f"Error: {data['message']}")
+        #if data['status'] == '1':
+        #    self.balance = int(data['result']) / 1e18  # Convert balance from wei to BNB
+        #    return self.balance
+        #else:
+        #    raise ValueError(f"Error: {data['message']}")
+
+            if data['status'] == '1':
+                balance = int(data['result']) / 1e18  # Convert balance from wei to BNB
+                balances[address] = balance
+                print(balances)
+            else:
+                balances[address] = None
+
+        return balances
